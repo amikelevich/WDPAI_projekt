8077a1b5c0dadaae144833d0cbbb9491d37f4352
amikelevich <a.mikelevich@student.pk.edu.pl>
2025-01-26 11:13:33 +0100

    Removed unused files

--- lab_1/ui-app/src/components/Register.test.tsx
+++ lab_1/ui-app/src/components/Register.test.tsx
@@ -13,7 +13,6 @@ describe('Register Component', () => {
     render(<Register />);
     render(<DummyComponent />);
 
-    // Check if form elements exist
     expect(screen.getByLabelText(/User name/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/User mail/i)).toBeInTheDocument();
     expect(screen.getByTestId('register-password')).toBeInTheDocument();
@@ -25,34 +24,28 @@ describe('Register Component', () => {
   it('shows an error when passwords do not match', async () => {
     render(<Register />);
 
-    // Simulate user input
     fireEvent.change(screen.getByLabelText(/User name/i), { target: { value: 'John Doe' } });
     fireEvent.change(screen.getByLabelText(/User mail/i), { target: { value: 'john.doe@example.com' } });
     fireEvent.change(screen.getByTestId('register-password'), { target: { value: 'password123' } });
     fireEvent.change(screen.getByTestId('register-confirmPassword'), { target: { value: 'differentPassword' } });
     fireEvent.click(screen.getByLabelText(/Accept our beautiful statement/i));
 
-    // Submit the form
     fireEvent.click(screen.getByRole('button', { name: /submit/i }));
 
-    // Check for the error message
     expect(await screen.findByText(/Passwords do not match./i)).toBeInTheDocument();
   });
 
   it('shows an error for invalid email', async () => {
     render(<Register />);
 
-    // Simulate user input
     fireEvent.change(screen.getByLabelText(/User name/i), { target: { value: 'John Doe' } });
     fireEvent.change(screen.getByLabelText(/User mail/i), { target: { value: 'invalid-email@xd' } });
     fireEvent.change(screen.getByTestId('register-password'), { target: { value: 'password123' } });
     fireEvent.change(screen.getByTestId('register-confirmPassword'), { target: { value: 'password123' } });
     fireEvent.click(screen.getByLabelText(/Accept our beautiful statement/i));
 
-    // Submit the form
     fireEvent.click(screen.getByRole('button', { name: /submit/i }));
 
-    // Check for the error message
     expect(screen.getByText(/Invalid email address./)).toHaveClass("error")
   });
 
